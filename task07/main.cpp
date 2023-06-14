92fc8200623706ecb2bba6b8d8c6b795da182e05
annan tang <annan.tang.china@gmail.com>
2023-06-14 15:24:39 +0900

    task07 finished

--- task07/main.cpp
+++ task07/main.cpp
@@ -26,6 +26,7 @@ void solve_laplace_gauss_sidel_on_grid(
       if (vtx2isfix[idx_center]) { continue; }
       // write some code below to implement Gauss-Sidel method
       // Do not write more than 5 lines of code
+      vtx2val[iy*grid_size+ix]= 1.0/4.0*(vtx2val[iy*grid_size + ix+1] + vtx2val[iy*grid_size + ix-1] + vtx2val[(iy+1)*grid_size+ix] + vtx2val[(iy-1)*grid_size+ix]);
     }
   }
 }
